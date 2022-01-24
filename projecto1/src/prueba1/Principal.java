fd49751359fb12f688c68a5fef0f063f10790dd9
usu2 <pepitogrillo@gmail.com>
2022-01-24 08:41:17 +0100

    Cambios en clase principal. rama creada

--- projecto1/src/prueba1/Principal.java
+++ projecto1/src/prueba1/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java_git");
 		System.out.println("Adios");
+		System.out.println("Clase principal");
+		System.out.println("Prueba Git. Clase principal");
 
 	}
 

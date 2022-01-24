a0009152eff30a020843d19ad086c7c2a13cdfb3
usu2 <pepitogrillo@gmail.com>
2022-01-24 08:40:45 +0100

    Fichero Cambio1 creado

---
+++ projecto1/src/cambio1/Cambio1.java
@@ -0,0 +1,11 @@
+package cambio1;
+
+public class Cambio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Prueba Git. Clase cambio1");
+	}
+
+}
